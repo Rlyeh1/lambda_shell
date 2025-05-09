d9e7cd44cb17683cf38001a51759738e6caf2cf3
Santiago <andresnavarro3@hotmail.com>
2025-05-08 22:24:00 -0500

    parameterized input query

--- shell.mjs
+++ shell.mjs
@@ -1,5 +1,7 @@
 import mysql from 'mysql2/promise';
 export const handler = async (event) =>{
+	const body = JSON.parse(event.body);
+	const {name} = body;
 
 	const connection = await mysql.createConnection({
 		host: 'myhost1',
@@ -7,7 +9,7 @@ export const handler = async (event) =>{
 		password: 'myPassword1',
 		database: 'myDatabase1',
 	});
-	const [rows] = await connection.execute('SELECT * FROM users');
+	const [rows] = await connection.execute('SELECT * FROM users where name = ?', [name]);
 	await connection.end();
 	return{
 		statusCode:200,

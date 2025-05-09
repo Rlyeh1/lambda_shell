b0c5b1fe9d48f2eb7a853b881bcf809f449ccaf0
Santiago <andresnavarro3@hotmail.com>
2025-05-08 21:59:47 -0500

    initial commit

---
+++ shell.mjs
@@ -0,0 +1,16 @@
+import mysql from 'mysql2/promise';
+export const handler = async (event) =>{
+
+	const connection = await mysql.createConnection({
+		host: 'myhost1',
+		user: 'myUser1',
+		password: 'myPassword1',
+		database: 'myDatabase1',
+	});
+	const [rows] = await connection.execute('SELECT * FROM users');
+	await connection.end();
+	return{
+		statusCode:200,
+		body: JSON.stringify(rows),
+	};
+};
